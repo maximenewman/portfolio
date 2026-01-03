7f77b30aefe395811d8a88fc92fc90f227de8cf2
maximenewman <mnn6@sfu.ca>
2026-01-03 02:55:13 -0800

    Initial mapping complete, now moving on to styling data

--- app/projects/data/projects.tsx
+++ app/projects/data/projects.tsx
@@ -1,11 +1,131 @@
-interface project {
+export interface project {
     title: string
     description: string[]
     tech: string[]
     link: string
 }
 
-const projects: project[] = [
+export const projects: project[] = [
+    {
+        title: "Goblin's Keep",
+        description: [
+            ` Led backend development of a 2D tile-based escape game, 
+            architecting game logic and AI systems
+            while coordinating code reviews and integration with teammates`,
+            ` Implemented dynamic goblin AI using modified A* pathfinding 
+            and randomized patrol logic,
+            enhancing difficulty balance and replayability`,
+            `Refactored core classes (MapGenerator, CollisionChecker, Entity) to reduce God classes, 
+            eliminate duplicated code, and improve maintainability following OOP best practices.`,
+            `Engineered unit and integration testing suite with JUnit and Mockito, 
+            reaching 96% line coverage and 92% branch coverage to ensure code reliability`,
+            `Centralized collision detection for tiles, objects, and entities, 
+            improving consistency in gameplay interactions and debugging efficiency.`
+        ],
+        tech: [
+            "Java",
+            "Maven",
+            "JUnit",
+            "Mockito"
+        ],
+        link: "https://github.com/maximenewman/GoblinsKeep"
+    },
+    {
+        title: "Goblin's Keep",
+        description: [
+            ` Led backend development of a 2D tile-based escape game, 
+            architecting game logic and AI systems
+            while coordinating code reviews and integration with teammates`,
+            ` Implemented dynamic goblin AI using modified A* pathfinding 
+            and randomized patrol logic,
+            enhancing difficulty balance and replayability`,
+            `Refactored core classes (MapGenerator, CollisionChecker, Entity) to reduce God classes, 
+            eliminate duplicated code, and improve maintainability following OOP best practices.`,
+            `Engineered unit and integration testing suite with JUnit and Mockito, 
+            reaching 96% line coverage and 92% branch coverage to ensure code reliability`,
+            `Centralized collision detection for tiles, objects, and entities, 
+            improving consistency in gameplay interactions and debugging efficiency.`
+        ],
+        tech: [
+            "Java",
+            "Maven",
+            "JUnit",
+            "Mockito"
+        ],
+        link: "https://github.com/maximenewman/GoblinsKeep"
+    },
+    {
+        title: "Goblin's Keep",
+        description: [
+            ` Led backend development of a 2D tile-based escape game, 
+            architecting game logic and AI systems
+            while coordinating code reviews and integration with teammates`,
+            ` Implemented dynamic goblin AI using modified A* pathfinding 
+            and randomized patrol logic,
+            enhancing difficulty balance and replayability`,
+            `Refactored core classes (MapGenerator, CollisionChecker, Entity) to reduce God classes, 
+            eliminate duplicated code, and improve maintainability following OOP best practices.`,
+            `Engineered unit and integration testing suite with JUnit and Mockito, 
+            reaching 96% line coverage and 92% branch coverage to ensure code reliability`,
+            `Centralized collision detection for tiles, objects, and entities, 
+            improving consistency in gameplay interactions and debugging efficiency.`
+        ],
+        tech: [
+            "Java",
+            "Maven",
+            "JUnit",
+            "Mockito"
+        ],
+        link: "https://github.com/maximenewman/GoblinsKeep"
+    },
+    {
+        title: "Goblin's Keep",
+        description: [
+            ` Led backend development of a 2D tile-based escape game, 
+            architecting game logic and AI systems
+            while coordinating code reviews and integration with teammates`,
+            ` Implemented dynamic goblin AI using modified A* pathfinding 
+            and randomized patrol logic,
+            enhancing difficulty balance and replayability`,
+            `Refactored core classes (MapGenerator, CollisionChecker, Entity) to reduce God classes, 
+            eliminate duplicated code, and improve maintainability following OOP best practices.`,
+            `Engineered unit and integration testing suite with JUnit and Mockito, 
+            reaching 96% line coverage and 92% branch coverage to ensure code reliability`,
+            `Centralized collision detection for tiles, objects, and entities, 
+            improving consistency in gameplay interactions and debugging efficiency.`
+        ],
+        tech: [
+            "Java",
+            "Maven",
+            "JUnit",
+            "Mockito"
+        ],
+        link: "https://github.com/maximenewman/GoblinsKeep"
+    },
+    {
+        title: "Goblin's Keep",
+        description: [
+            ` Led backend development of a 2D tile-based escape game, 
+            architecting game logic and AI systems
+            while coordinating code reviews and integration with teammates`,
+            ` Implemented dynamic goblin AI using modified A* pathfinding 
+            and randomized patrol logic,
+            enhancing difficulty balance and replayability`,
+            `Refactored core classes (MapGenerator, CollisionChecker, Entity) to reduce God classes, 
+            eliminate duplicated code, and improve maintainability following OOP best practices.`,
+            `Engineered unit and integration testing suite with JUnit and Mockito, 
+            reaching 96% line coverage and 92% branch coverage to ensure code reliability`,
+            `Centralized collision detection for tiles, objects, and entities, 
+            improving consistency in gameplay interactions and debugging efficiency.`
+        ],
+        tech: [
+            "Java",
+            "Maven",
+            "JUnit",
+            "Mockito"
+        ],
+        link: "https://github.com/maximenewman/GoblinsKeep"
+    },
     {
         title: "Goblin's Keep",
         description: [
@@ -31,7 +151,3 @@ const projects: project[] = [
         link: "https://github.com/maximenewman/GoblinsKeep"
     },
 ]
-
-export default function project_mapper(){
-    
-}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,34 @@
-import { project, projects } from "./data"
+import { project, projects} from "./data/projects"
 
 export default function Portfoliorojects() {
     return (
-        <div>
-            <h1>Projects</h1>
-            <h2></h2>
+        <div className="container">
+            <div></div>
+
+            <div className="flex items-center justify-center">
+                {projects.map((project, index) => (
+                    <div key={index} className="py-5">
+                        <h1><strong>{project.title}</strong></h1>
+
+                        <h2><strong>Description</strong></h2>
+                        <ul>
+                            {project.description.map((desc, i) => (
+                                <li key={i}>{desc}</li>
+                            ))}
+                        </ul>
+
+                        <h2><strong>Technologies</strong></h2>
+                        <ul>
+                            {project.description.map((tech, i) => (
+                                <li key={i}>{tech}</li>
+                            ))}
+                        </ul>
+                        
+                        <a href={project.link}><strong>View Project</strong></a>
+
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
