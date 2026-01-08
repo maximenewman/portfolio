04f37c270b845aa10ad47b4cb59ce92140ada299
maximenewman <mnn6@sfu.ca>
2026-01-07 16:49:39 -0800

    Refactored display_experience, now function can be reused

---
+++ app/projects/components/project_display.tsx
@@ -0,0 +1,34 @@
+import { project } from "@/app/projects/data/projects"
+
+export function display_project(projects: project[]){
+    return (
+        <div className="container">
+            <div></div>
+
+            <div className="flex flex-wrap items-center justify-center">
+                {projects.map((project, index) => (
+                    <div key={index} className="my-5">
+                        <h1><strong>{project.title}</strong></h1>
+                        
+                        <h2><strong>Description</strong></h2>
+                        <ul className="list-disc list-inside">
+                            {project.description.map((desc, i) => (
+                                <li key={i}>{desc}</li>
+                            ))}
+                        </ul>
+
+                        <h2><strong>Technologies</strong></h2>
+                        <ul className="list-disc list-inside">
+                            {project.tech.map((tech, i) => (
+                                <li key={i}>{tech}</li>
+                            ))}
+                        </ul>
+                        
+                        <a href={project.link}><strong>View Project</strong></a>
+
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,34 +1,8 @@
-import { project, projects} from "./data/projects"
+import { projects} from "./data/projects"
+import { display_project } from "./components/project_display"
 
 export default function Portfoliorojects() {
     return (
-        <div className="container">
-            <div></div>
-
-            <div className="flex flex-wrap items-center justify-center">
-                {projects.map((project, index) => (
-                    <div key={index} className="my-5">
-                        <h1><strong>{project.title}</strong></h1>
-                        
-                        <h2><strong>Description</strong></h2>
-                        <ul className="list-disc list-inside">
-                            {project.description.map((desc, i) => (
-                                <li key={i}>{desc}</li>
-                            ))}
-                        </ul>
-
-                        <h2><strong>Technologies</strong></h2>
-                        <ul className="list-disc list-inside">
-                            {project.tech.map((tech, i) => (
-                                <li key={i}>{tech}</li>
-                            ))}
-                        </ul>
-                        
-                        <a href={project.link}><strong>View Project</strong></a>
-
-                    </div>
-                ))}
-            </div>
-        </div>
+        display_project(projects)
     )
 }
