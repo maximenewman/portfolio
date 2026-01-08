f8fbf64a1df176f4c914b8ce3fbc6bd840e3153b
maximenewman <mnn6@sfu.ca>
2026-01-07 23:25:44 -0800

    Structured experiences, now need to complete styling so that projects aren't indented

--- app/experiences/components/display_experiences.tsx
+++ app/experiences/components/display_experiences.tsx
@@ -3,12 +3,16 @@ import { experience } from "@/app/experiences/data/experience"
 
 export function display_experience(experiences: experience[]){
     return (
-        <div className="container">
+        <div className="container flex flex-col items-center">
             {experiences.map( (experience, index) => (
-                
-
+                <div key={index} className="border border-black mx-2">
+                    <h2><strong>Role:</strong> {experience.role}</h2>
+                    <h2><strong>Company:</strong> {experience.company}</h2>
+                    <h2><strong>Date:</strong> {experience.date}</h2>
+                    <h2><strong>Location:</strong> {experience.location}</h2>
+                    {display_project(experience.projects)}
+                </div>
             ))}
-
         </div>
     )
 }
