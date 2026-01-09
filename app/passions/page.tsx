ed2fa0ce9466bbd1d5df3b42a258be28129f342b
maximenewman <mnn6@sfu.ca>
2026-01-08 20:22:45 -0800

    Temp holder for passions

--- app/passions/page.tsx
+++ app/passions/page.tsx
@@ -1,8 +1,8 @@
 export default function Passions(){
     return (
         <div>
-            <header>
-                <h1>My Passions</h1>
+            <header className="hero">
+                <h1 className="hero-content text-center text-5xl font-bold">My Passions: Coming Soon</h1>
             </header>
         </div>
     )
